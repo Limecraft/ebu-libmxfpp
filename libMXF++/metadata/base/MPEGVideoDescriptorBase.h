23dc172e32aaf75e63d9d1710816e4164b8535b2
Philip de Nier <philipn@rd.bbc.co.uk>
2015-12-10 12:24:29 +0000

    renamed MPEGVideoDescriptor::MaxBPictureCount

--- libMXF++/metadata/base/MPEGVideoDescriptorBase.h
+++ libMXF++/metadata/base/MPEGVideoDescriptorBase.h
@@ -68,8 +68,8 @@ public:
    bool getIdenticalGOP() const;
    bool haveMaxGOP() const;
    uint16_t getMaxGOP() const;
-   bool haveBPictureCount() const;
-   uint16_t getBPictureCount() const;
+   bool haveMaxBPictureCount() const;
+   uint16_t getMaxBPictureCount() const;
    bool haveBitRate() const;
    uint32_t getBitRate() const;
    bool haveProfileAndLevel() const;
@@ -85,7 +85,7 @@ public:
    void setClosedGOP(bool value);
    void setIdenticalGOP(bool value);
    void setMaxGOP(uint16_t value);
-   void setBPictureCount(uint16_t value);
+   void setMaxBPictureCount(uint16_t value);
    void setBitRate(uint32_t value);
    void setProfileAndLevel(uint8_t value);
 
